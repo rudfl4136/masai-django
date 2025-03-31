627fcf78215daf2c7cf6cadfcfc349b6b562a412
masstudio <masstdio@gmail.com>
2025-03-31 20:41:56 +0900

    urls.py  수정

--- masaisite/urls.py
+++ masaisite/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     ## 초기 셋팅
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('masai/', include('masai.urls')),
     path('admin/', admin.site.urls),
 ]
